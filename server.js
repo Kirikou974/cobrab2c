5c22b73cd308cb124ccc9fc052732343652142a2
thierrybigot <thierrybigot@one-share.fr>
2017-07-03 15:45:43 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,35 +1,35 @@
-
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require("request");
+var session = require('express-session');
+var authOKTA = require('./authOKTA');
+var authAZURE = require('./authAzure');
+var url = require('url');
+var saml2json = require('saml2json');
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
-});
+var port = process.env.port || process.env.PORT || 8080;
+app.get('/azure', function(req, res) {
+    var clientID = 'aa69bc8f-93d6-45c9-848f-ea3d9efa5c06';
+    var authority = 'https://login.microsoftonline.com/tfp/testsdxb2c.onmicrosoft.com/B2C_1_SodexoB2CAPPPolicy';
+    var b2cScopes = 'https://testsdxb2c.onmicrosoft.com/sodexob2capp/TestB2C';
+    if(port == 8080)
+    {
+        clientID ='4d74ca44-3825-481f-8ae7-61f1fae9bd30';
+        authority ='https://login.microsoftonline.com/tfp/oneshareb2c.onmicrosoft.com/B2C_1_Profile_Create';
+        b2cScopes = 'https://oneshareb2c.onmicrosoft.com/TestB2C/TestB2CSCope1';
+    }
+    res.render('azureb2c.ejs', { 
+        clientID: clientID,
+        authority: authority,
+        b2cScopes: b2cScopes,
+        webApi: 'https://fabrikamb2chello.azurewebsites.net/hello'
+    });
+}).get('/ping', function(req, res){
+    res.render('ping.ejs');
+}).use(function(req, res, next){
+    res.status('404').send('Page introuvable. Essayez /okta, /azure ou /ping');
+})
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
-
-app.get('/', routes.index);
-app.get('/users', user.list);
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
-});
+app.listen(port);
