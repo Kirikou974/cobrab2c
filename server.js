5798a49d27b4377c7b427134240d777385a26741
thierrybigot <thierrybigot@one-share.fr>
2017-07-05 11:42:37 +0200

    added cors

--- server.js
+++ server.js
@@ -1,13 +1,7 @@
-require('babel-register')({
-  presets: ['react']
-});
-
 var express = require('express');
 var request = require('request');
 var morgan = require('morgan')
-var React = require('react');
-var ReactDOMServer = require('react-dom/server');
-var SignInComponent = require('./containers/signin.jsx');
+var cors = require('cors');
 var config = require('./config');
 
 var app = express();
@@ -16,14 +10,13 @@ var signup = 'signup';
 var verifyCobraCode = 'verifyCobraCode';
 
 app.use(morgan('tiny'));
+app.use(cors());
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.redirect('/' + signin);
 }).get('/' + signin,function(req, res) {
-  var html = ReactDOMServer.renderToString(
-    React.createElement(SignInComponent)
-  )
+  var html = "<div id='app'></div>"
   res.send(html);
 }).post('/' + signin, function(req, res) {
   res.send('Sign in');
